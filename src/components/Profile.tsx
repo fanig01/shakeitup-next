e83b3fb99812017bf6a51435bcd3f0559b8d70ff
Fagner Rodrigues <fanig_01@hotmail.com>
2021-02-26 15:52:03 -0300

    created leveling structure with new context, added logic for complete and failure challenges and added web notifications

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/4106608?s=400&u=4906ee63db8ebc6363f1a8c21c86a8f5f07f5a44&v=4" alt="Fagner Rodrigues"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Fagner Rodrigues</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
